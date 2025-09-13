ec4c2fd03958d86fca4c3ecd118390d79ee1a9c9
Lucas Flores <lafa.dev@protonmail.com>
2025-09-13 15:10:02 -0300

    update database potions

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -2,17 +2,17 @@ import 'dotenv/config';
 import type { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'raziel',
+  username: process.env.DB_USER || 'your_admin_user',
   password: process.env.DB_PASSWORD || 'your_password',
   database: process.env.DB_NAME || 'wedding_db',
   host: process.env.DB_HOST || 'wedding-db.database.windows.net',
-  port: Number(process.env.DB_PORT) || 1433, // important!
-  dialect: 'mysql',
+  port: Number(process.env.DB_PORT) || 1433,
+  dialect: 'mssql',
   dialectOptions: {
-    ssl: {
-      rejectUnauthorized: true, // required for Azure MySQL SSL
+    options: {
+      encrypt: true, // required for Azure
+      trustServerCertificate: false,
     },
-    timezone: 'Z',
   },
   logging: false,
 };
