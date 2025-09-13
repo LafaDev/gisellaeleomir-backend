6fb239addbcffc6a08ebbcba37ca40c5484a1775
Lucas Flores <lafa.dev@protonmail.com>
2025-09-13 06:42:07 -0300

    databese config update

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -2,13 +2,16 @@ import 'dotenv/config';
 import type { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USER || 'raziel',
+  password: process.env.DB_PASSWORD || 'your_password',
+  database: process.env.DB_NAME || 'wedding_db',
+  host: process.env.DB_HOST || 'wedding-db.database.windows.net',
+  port: Number(process.env.DB_PORT) || 1433, // important!
   dialect: 'mysql',
   dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true, // required for Azure MySQL SSL
+    },
     timezone: 'Z',
   },
   logging: false,
